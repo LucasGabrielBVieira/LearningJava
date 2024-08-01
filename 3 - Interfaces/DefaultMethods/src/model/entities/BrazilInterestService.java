53ca6517a35fbce8eaaab7fead7e722bfc415ba9
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-08-01 13:41:55 -0300

    Correção do diretório

--- 3 - Interfaces/DefaultMethods/src/model/entities/BrazilInterestService.java
+++ 3 - Interfaces/DefaultMethods/src/model/entities/BrazilInterestService.java
@@ -1,13 +1,14 @@
 package model.entities;
 
-public class UsaInterestService implements InterestService {
+public class BrazilInterestService implements InterestService {
 
     private double interestRate;
 
-    public UsaInterestService(double interestRate) {
+    public BrazilInterestService(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    @Override
     public double getInterestRate() {
         return interestRate;
     }
--- 3 - Interfaces/DefaultMethods/src/model/entities/UsaInterestService.java
+++ 3 - Interfaces/DefaultMethods/src/model/entities/UsaInterestService.java
@@ -1,13 +1,14 @@
 package model.entities;
 
-public class BrazilInterestService implements InterestService {
+public class UsaInterestService implements InterestService {
 
     private double interestRate;
 
-    public BrazilInterestService(double interestRate) {
+    public UsaInterestService(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    @Override
     public double getInterestRate() {
         return interestRate;
     }
